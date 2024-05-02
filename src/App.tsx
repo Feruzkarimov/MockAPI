851a66341083f9333a2d9bfbf2a4a27312513cd1
Feruzkarimov <feruzbekkarimov02@gmail.com>
2024-05-02 11:46:30 +0500

    feat(created: Theme Mode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { ThemeProvider } from "@/components/theme-provider"
 
-const App = () => {
-  return <div>App</div>  
+function App() {
+  return (
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      {children}
+    </ThemeProvider>
+  )
 }
 
 export default App
